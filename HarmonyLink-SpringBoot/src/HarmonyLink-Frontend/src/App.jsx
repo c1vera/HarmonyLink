5afc20447b1410d3225e66481a9a18fc35e066c8
Vera <yahwa2009@naver.com>
2024-01-31 19:18:31 +0900

    test 요청

--- HarmonyLink-SpringBoot/src/HarmonyLink-Frontend/src/App.jsx
+++ HarmonyLink-SpringBoot/src/HarmonyLink-Frontend/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <>
-      <p>테스트중</p>
-    </>
-  )
+    const [hello, setHello] = useState('');
+
+    useEffect(() => {
+        axios.get('/api/test')
+            .then((res) => {
+                setHello(res.data);
+            })
+    }, []);
+    return (
+        <div className="App">
+            백엔드 데이터 : {hello}
+        </div>
+    );
 }
 
-export default App
+export default App;
