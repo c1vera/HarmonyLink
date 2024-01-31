ec42584887ead6c37ee17196a2e9b935f37a4f35
jakdalmak <jakdalmak7961@gmail.com>
2024-01-31 19:03:30 +0900

    add springboot project

---
+++ HarmonyLink-SpringBoot/src/main/java/com/dongyang/HarmonyLink/HarmonyLinkApplication.java
@@ -0,0 +1,13 @@
+package com.dongyang.HarmonyLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyLinkApplication.class, args);
+	}
+
+}
---
+++ HarmonyLink-SpringBoot/src/test/java/com/dongyang/HarmonyLink/HarmonyLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongyang.HarmonyLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
