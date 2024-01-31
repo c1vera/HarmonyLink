0448e1abe5339a98f432c67178f5fe881f1598b4
jakdalmak <jakdalmak7961@gmail.com>
2024-02-01 02:12:39 +0900

    회원가입_기능만 구현

--- HarmonyLink-SpringBoot/src/main/java/com/dongyang/HarmonyLink/HarmonyLinkApplication.java
+++ HarmonyLink-SpringBoot/src/main/java/com/dongyang/HarmonyLink/HarmonyLinkApplication.java
@@ -2,7 +2,9 @@ package com.dongyang.HarmonyLink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class HarmonyLinkApplication {
 
