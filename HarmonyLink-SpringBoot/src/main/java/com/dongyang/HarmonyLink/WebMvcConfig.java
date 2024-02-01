fb6629327ff701b476590bbdcaf2bdf044c879bf
jakdalmak <jakdalmak7961@gmail.com>
2024-02-01 18:46:39 +0900

    webconfig 다시 push 용도

--- HarmonyLink-SpringBoot/src/main/java/com/dongyang/HarmonyLink/WebMvcConfig.java
+++ HarmonyLink-SpringBoot/src/main/java/com/dongyang/HarmonyLink/WebMvcConfig.java
@@ -1,4 +1,20 @@
 package com.dongyang.HarmonyLink;
 
-public class WebMvcConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH");
+
+        // WebMvcConfigurer.super.addCorsMappings(registry);
+    }
 }
