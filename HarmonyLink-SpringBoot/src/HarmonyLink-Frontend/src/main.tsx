b59b5276c397bd21f308f2e3a48102a61ae88d3c
Vera <yahwa2009@naver.com>
2024-02-21 19:37:27 +0900

    새로고침시에도 cilent쪽 store 초기화되던 현상 fix

--- HarmonyLink-SpringBoot/src/HarmonyLink-Frontend/src/main.tsx
+++ HarmonyLink-SpringBoot/src/HarmonyLink-Frontend/src/main.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
-      </Provider>
-  </React.StrictMode>,
-)
+    </Provider>
+  </React.StrictMode>
+);
--- HarmonyLink-SpringBoot/src/HarmonyLink-Frontend/src/redux/store.ts
+++ HarmonyLink-SpringBoot/src/HarmonyLink-Frontend/src/redux/store.ts
@@ -1,16 +1,38 @@
-// src/redux/store.ts
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { userReducer } from './reducers/userReducer'; 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // localStorage를 사용할 저장소로 import
 
-import { configureStore } from '@reduxjs/toolkit';
-import { userReducer } from './reducers/userReducer';
+// Persist를 위한 설정
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'], // 여기에 유지하고 싶은 상태의 리듀서 이름을 배열 형태로 추가
+  // blacklist: [], // 유지하지 않을 상태의 리듀서 이름을 배열 형태로 추가
+};
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    // 다른 리듀서들을 여기에 추가
-  },
+// 리듀서들을 합치는 과정
+const rootReducer = combineReducers({
+  user: userReducer,
+  // 다른 리듀서들을 여기에 추가
 });
+
+// Persist 리듀서 설정
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// 스토어 생성
+const store = configureStore({
+  reducer: persistedReducer, // Persist가 적용된 리듀서를 사용
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+// Persistor 생성
+const persistor = persistStore(store);
 
 export type AppState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export default store;
+export { store, persistor };
