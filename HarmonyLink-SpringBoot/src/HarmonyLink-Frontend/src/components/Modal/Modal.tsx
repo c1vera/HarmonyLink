fed74c458067a38a916035df9a4ce86198b732ac
Vera <yahwa2009@naver.com>
2024-03-07 14:15:04 +0900

    feat: 재사용 가능한 모달 컴포넌트 생성

---
+++ HarmonyLink-SpringBoot/src/HarmonyLink-Frontend/src/components/Modal/Modal.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+
+interface ModalProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  buttonName?: string;
+  modalTitle?: string;
+}
+
+const ModalArea = styled.div`
+  font-family: "omyu_pretty";
+  font-size: x-large;
+`;
+
+const CustomModal: React.FC<ModalProps> = ({
+  buttonName,
+  modalTitle,
+  children,
+}) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        {buttonName}
+      </Button>
+      <Modal size="lg" show={show} onHide={handleClose}>
+        <ModalArea>
+          <Modal.Header closeButton>
+            <Modal.Title>{modalTitle}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{children}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </ModalArea>
+      </Modal>
+    </>
+  );
+};
+
+export default CustomModal;
