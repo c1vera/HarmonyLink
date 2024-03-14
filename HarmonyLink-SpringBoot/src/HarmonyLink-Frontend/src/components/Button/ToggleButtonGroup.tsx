e3e33cf78ec403ca1baa8136a4a148f655156018
Vera <yahwa2009@naver.com>
2024-03-14 10:43:02 +0900

    style: ToggleButtonGroup 컴포넌트 주석 삭제

--- HarmonyLink-SpringBoot/src/HarmonyLink-Frontend/src/components/Button/ToggleButtonGroup.tsx
+++ HarmonyLink-SpringBoot/src/HarmonyLink-Frontend/src/components/Button/ToggleButtonGroup.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { ButtonGroup, ToggleButton } from 'react-bootstrap';
 
-// Option 객체에 대한 인터페이스 정의
 interface Option {
   label: string | number;
   value: string | number;
   key: string | number;
 }
 
-// ToggleButtonGroup props에 대한 인터페이스 정의
 interface ToggleButtonGroupProps {
   options: Option[];
   name: string;
   value: string | number;  
-  onChange: (value: string) => void; // onChange 이벤트 핸들러의 타입을 정의
+  onChange: (value: string) => void;
 }
 
 const ToggleButtonGroup: React.FC<ToggleButtonGroupProps> = ({ options, name, value, onChange }) => {
