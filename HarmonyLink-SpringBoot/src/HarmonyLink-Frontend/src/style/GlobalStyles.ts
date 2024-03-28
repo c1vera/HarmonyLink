00e13028ce15f3c10a4a0d89d5ce64b5bc416639
Vera <yahwa2009@naver.com>
2024-03-28 16:25:44 +0900

    design: GlobalStyle Scroll Body 생성

--- HarmonyLink-SpringBoot/src/HarmonyLink-Frontend/src/style/GlobalStyles.ts
+++ HarmonyLink-SpringBoot/src/HarmonyLink-Frontend/src/style/GlobalStyles.ts
@@ -12,3 +12,14 @@ export const Body = styled.div`
   background-color: white;
   box-shadow: 0px 2px 10px 0px rgba(154, 154, 154, 0.559);
 `;
+
+export const ScrollBody = styled.div`
+  height: calc(100% - 70px);
+  
+  overflow-y: scroll;
+  margin: 40px;
+  padding: 40px;
+  border-radius: 20px;
+  background-color: white;
+  box-shadow: 0px 2px 10px 0px rgba(154, 154, 154, 0.559);
+`;
