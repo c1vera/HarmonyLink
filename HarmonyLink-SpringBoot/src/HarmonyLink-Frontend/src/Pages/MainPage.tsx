5637786c51162b44a54d571d43cf4ad186b79fa5
Vera <yahwa2009@naver.com>
2024-03-28 16:26:06 +0900

    feat: 세션 만료 확인용 버튼 추가

--- HarmonyLink-SpringBoot/src/HarmonyLink-Frontend/src/Pages/MainPage.tsx
+++ HarmonyLink-SpringBoot/src/HarmonyLink-Frontend/src/Pages/MainPage.tsx
@@ -1,12 +1,28 @@
 import styled from "styled-components";
 import "./MainPage.css";
 import * as S from "../style/GlobalStyles"
+import axios from 'axios';
 
-const MainPage: React.FC = () => {
+const MainPage = () => {
+
+  const checkSession = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/v1/session/validate', { withCredentials: true });
+      console.log('세션 유효: ', response);
+      // 세션 유효 시 추가적인 로직 처리
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        console.error('세션 만료 또는 유효하지 않음');
+        // 세션 만료 시 처리 로직, 예를 들어 로그인 페이지로 리다이렉션
+      }
+    }
+  };    
+  
   return (
     <div className="MainPage">
       <S.Body>
         <p>메인입니다</p>
+        <button onClick={checkSession}>세션확인</button>
       </S.Body>
     </div>
   );
